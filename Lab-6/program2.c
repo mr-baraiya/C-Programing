83e5921f511e9abac10299f39c8d27f65b4f93db
mr-baraiya <142961593+mr-baraiya@users.noreply.github.com>
2024-12-26 18:55:57 +0530

    Update program2.c

--- Lab-6/program2.c
+++ Lab-6/program2.c
@@ -1,7 +1,6 @@
-//2. Deƒne a union called Players which describe details like p_name, team, score, average. 
+//2. Deƒine a union called Players which describe details like p_name, team, score, average. 
 //Write a program to read data for 3 players and print that data. (A)
 #include <stdio.h>
-#include<string.h>
 
 union Player{
 	char p_name[50];
@@ -13,42 +12,49 @@ union Player{
 void main(){
 	int n=3;
 	union Player p[n];
-	
-	char Pname[n][50];
-	char Team[n][50];
-	int Score[n];
-	float Average[n];
-	
 	int i=0;
-	//Get Details of Players
+	//Get The Player Name
 	for(i=0;i<n;i++){
-		printf("Enter the Details Of Player : %d\n",(i+1));
-		printf("Enter the Player Name : ",(i+1));
-		if(i!=0){
-			gets(Pname[i]);
-		}
-		gets(Pname[i]);
-		printf("Enter the Team Name : ",(i+1));
-		gets(Team[i]);
-		printf("Enter the Score : ",(i+1));
-		scanf("%d",&Score[i]);
-		printf("Enter the Average : ",(i+1));
-		scanf("%f",&Average[i]);
+		printf("Enter the %d Player Name : ",(i+1));
+		gets(p[i].p_name);
 	}
-	//copy in union
+	//print the Payer Name
+	printf("\n");
 	for(i=0;i<n;i++){
-		strcpy(p[i].p_name,Pname[i]);
-		strcpy(p[i].team,Team[i]);
-		p[i].score = Score[i];
-		p[i].average = Average[i];
-	}
-	//print Student Details
-	for(i=0;i<n;i++){
+		printf("%d. %s",(i+1),p[i].p_name);
 		printf("\n");
-		printf("Player Name : %s \n",p[i].p_name);
-		printf("Team Name : %s \n",p[i].team);
-		printf("Score : %d \n",p[i].score);
-		printf("Average : %f \n",p[i].average);
+	}
+	//Get The Team Name
+	for(i=0;i<n;i++){
+		printf("Enter the %d Team Name : ",(i+1));
+		gets(p[i].team);
+	}
+	//print the Team Name
+	printf("\n");
+	for(i=0;i<n;i++){
+		printf("%d. %s",(i+1),p[i].team);
+		printf("\n");
+	}
+	//Get The Score
+	for(i=0;i<n;i++){
+		printf("Enter the %d Player Score : ",(i+1));
+		scanf("%d",&p[i].score);
+	}
+	//print the Score
+	printf("\n");
+	for(i=0;i<n;i++){
+		printf("%d. %d",(i+1),p[i].score);
+		printf("\n");
+	}
+	//Get The Average
+	for(i=0;i<n;i++){
+		printf("Enter the %d Player Average : ",(i+1));
+		scanf("%f",&p[i].average);
+	}
+	//print the Average
+	printf("\n");
+	for(i=0;i<n;i++){
+		printf("%d. %f",(i+1),p[i].average);
 		printf("\n");
 	}
 }
