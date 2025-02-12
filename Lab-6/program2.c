a3eb95e0bf3fb7fbb899f28db4b559c1ea8fa904
mr-baraiya <142961593+mr-baraiya@users.noreply.github.com>
2025-02-12 22:35:37 +0530

    Update program2.c

--- Lab-6/program2.c
+++ Lab-6/program2.c
@@ -1,36 +1,28 @@
 //2. Deƒine a union called Players which describe details like p_name, team, score, average. 
 //Write a program to read data for 3 players and print that data. (A)
 #include <stdio.h>
-
 union Player{
 	char p_name[50];
 	char team[50];
 	int score;
 	float average;
 };
-
 void main(){
-	int n=3;
-	union Player p[n];
+	union Player p[3];
 	int i=0;
-	
-	for(i=0;i<n;i++){
+	for(i=0;i<3;i++){
 		printf("\n");
-		
 		printf("Enter the Details Of Player : %d\n",(i+1));
-		printf("Enter the Player Name : ",(i+1));
+		printf("Enter the Player Name : ");
 		gets(p[i].p_name);
-		
 		printf("Player Name : %s \n",p[i].p_name);
-		printf("Enter the Team Name : ",(i+1));
+		printf("Enter the Team Name : ");
 		gets(p[i].team);
-		
 		printf("Team Name : %s \n",p[i].team);
-		printf("Enter the Score : ",(i+1));
+		printf("Enter the Score : ");
 		scanf("%d",&p[i].score);
-		
 		printf("Score : %d \n",p[i].score);
-		printf("Enter the Average : ",(i+1));
+		printf("Enter the Average : ");
 		scanf("%f",&p[i].average);
 		printf("Average : %f \n",p[i].average);
 		
