0fbcb14ca1cefa6ef5cbc558d6fccbad39bad2b4
mr-baraiya <142961593+mr-baraiya@users.noreply.github.com>
2025-02-22 19:26:24 +0530

    Update program2.c

--- Lab-10/program2.c
+++ Lab-10/program2.c
@@ -13,12 +13,15 @@ void main(){
 		return;
 	}
 	
+//	p[i] = *(p+i)
+//	&p[i] = (p+i)
+
 	for(i=0;i<n;i++){
 		printf("Enter the Number in %d : ",i);
-		scanf("%d",&p[i]);
+		scanf("%d",(p+i));
 	}
 	for(i=0;i<n;i++){
-		sum = sum + p[i];
+		sum = sum + p[i]);
 	}
 	printf("Sum = %d",sum);
 	// Deallocate memory
