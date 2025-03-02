99a75507b40485176bca2116d962f47c092c7003
mr-baraiya <142961593+mr-baraiya@users.noreply.github.com>
2025-03-02 16:56:29 +0530

    Update program3.c

--- Lab-10/program3.c
+++ Lab-10/program3.c
@@ -12,12 +12,14 @@ void main(){
 		printf("Memory Allocation Failed.\n");
 		return;
 	} 
+	
 	for (i=0;i<n;i++){
 		printf("Enter the number in %d : ",i);
-		scanf("%d",&p[i]);
+		scanf("%d",(p+i));
 	}
+	
 	for (i=0;i<n;i++){
-		sum = sum + p[i];
+		sum = sum + *(p+i);
 	}
 	printf("Sum = %d",sum);
 	// Deallocate memory
