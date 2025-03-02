12025902d85c5743dfd2d6d48367a4138f2351a4
mr-baraiya <142961593+mr-baraiya@users.noreply.github.com>
2025-03-02 16:56:06 +0530

    Update program1.c

--- Lab-10/program1.c
+++ Lab-10/program1.c
@@ -3,18 +3,20 @@
 #include <stdlib.h>
 
 void main(){
-	char *p_char;
-	int *p_int;
-	float *p_float;
+	char 	*p_char;
+	int 	*p_int;
+	float 	*p_float;
+	
 	//Aloocate Memory
-	p_char = (char *) malloc(sizeof(char));
-	p_int = (int *) malloc(sizeof(int));
-	p_float = (float *) malloc(sizeof(float));
+	p_char 	= 	(char 	*) 	malloc(sizeof(char));
+	p_int 	= 	(int 	*) 	malloc(sizeof(int));
+	p_float = 	(float 	*) 	malloc(sizeof(float));
 	
 	if(p_char == NULL || p_int == NULL || p_float == NULL){
 		printf("Memmory Allocation Is Failed.\n");
 		return;
 	}
+	
 	printf("Enter The Character : ");
 	scanf("%c",p_char);
 	printf("Enter the Integer : ");
@@ -22,7 +24,10 @@ void main(){
 	printf("Enter the Float Number : ");
 	scanf("%f",p_float);
 	
-	printf(" p_char = %c \n p_int = %d \n p_float = %f \n",*p_char,*p_int,*p_float);
+	printf(" p_char  = %c \n",*p_char);
+	printf(" p_int   = %d \n",*p_int);
+	printf(" p_float = %.2f \n",*p_float);
+	
 	//Free Memory
 	free(p_char);
 	free(p_int);
