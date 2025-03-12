2825fa10355a2b7b42395f89f5326bdc953d6e56
mr-baraiya <142961593+mr-baraiya@users.noreply.github.com>
2025-03-12 22:54:20 +0530

    Update demo.c

--- Lab-9/demo.c
+++ Lab-9/demo.c
@@ -10,31 +10,66 @@ void main(){
 	int arr2[m][n];
 	int i=0,j=0;
 	
+	//calculate the sum of arr1,arr2
 	int sum [m][n];
 	int *p1 = &arr1;
-	int *p2 = arr2;
-	//int *p3 = &sum;
+	int *p2 = &arr2;
+	int *p3 = &sum;
 	
-
-	printf("\n\nWith n:\n");
+	//get the first Array
+	printf("\nEnter the first Array : \n");
 	for(i=0;i<m;i++){
 		for(j=0;j<n;j++){
-			printf("%d\t",(p1+i*n+j));
+			printf("Enter the arr[%d][%d] : ",i,j);
+			//scanf("%d",&arr1[i][j]);
+			scanf("%d",(p1+i*n+j));
 		}
 		printf("\n");
-	}	
-}
+	}
 	
-//	for(i=0;i<m;i++){
-//		for(j=0;j<n;j++){
-//			scanf("%d",(p1+i*n+j));
-//		}
-//	}
-//	
-//	for(i=0;i<m;i++){
-//		for(j=0;j<n;j++){
-//			printf("%d\t",*(p1+i*n+j));
-//		}
-//		printf("\n");
-//	}	
-//}
+	//get the Second Array
+	printf("\nEnter the second Array : \n");
+	for(i=0;i<m;i++){
+		for(j=0;j<n;j++){
+			printf("Enter the arr[%d][%d] : ",i,j);
+			//scanf("%d",&arr2[i][j]);
+			scanf("%d",(p2+i*n+j));
+		}
+		printf("\n");
+	}
+	
+	for(i=0;i<m;i++){
+		for(j=0;j<n;j++){
+			//sum[i][j] = arr1[i][j] + arr2[i][j];
+			*(p3+i*n+j) = *(p1+i*n+j) + *(p2+i*n+j);
+		}
+	}
+	
+	//print the Matrices
+	printf("Matrix 1 : \n");
+	for(i=0;i<m;i++){
+		for(j=0;j<n;j++){
+			//printf("%d ",arr1[i][j]);
+			printf("%d ",*(p1+i*n+j));
+		}
+		printf("\n");
+	}
+	
+	printf("Matrix 2 : \n");
+	for(i=0;i<m;i++){
+		for(j=0;j<n;j++){
+			//printf("%d ",arr2[i][j]);
+			printf("%d ",*(p2+i*n+j));
+		}
+		printf("\n");
+	}
+	
+	printf("Sum : \n");
+	for(i=0;i<m;i++){
+		for(j=0;j<n;j++){
+			//printf("%d ",sum[i][j]);
+			printf("%d ",*(p3+i*n+j));
+		}
+		printf("\n");
+	}
+}
